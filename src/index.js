eb55e775cfa9ce9d5c848d5c09612e6da811d283
Nikita Ustinov <65974693+NikitaUstnov@users.noreply.github.com>
2021-06-30 12:26:47 +0300

    3 sections

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-import './sass/main.scss';
+import './less/main.less';
+const mySiema = new Siema();
+
+//for header
+const openHederNavMenuBtnRef = document.querySelector('.open-js');
+const closeHederNavMenuBtnRef = document.querySelector('.close-js');
+const siteNavRef = document.querySelector('.site-nav ');
+
+openHederNavMenuBtnRef.addEventListener('click', () => {
+  siteNavRef.classList.add('is-open');
+  closeHederNavMenuBtnRef.classList.add('is-open');
+});
+
+closeHederNavMenuBtnRef.addEventListener('click', () => {
+  siteNavRef.classList.remove('is-open');
+  closeHederNavMenuBtnRef.classList.remove('is-open');
+});
+
+//for main
+document.querySelector('.prev').addEventListener('click', () => mySiema.prev());
+document.querySelector('.next').addEventListener('click', () => mySiema.next());
